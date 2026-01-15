983a0237e020e954bf4ff25c36b791e58f07d067
Alirio Rivera <ariveracu@unal.edu.co>
2026-01-14 23:44:43 -0500

    adding node

---
+++ Trees/Node.java
@@ -0,0 +1,14 @@
+package Trees;
+
+public class Node {
+
+    int data;
+    Node left;
+    Node right;
+
+    public Node(int value){
+        data = value;
+        left = null;
+        right = null;
+    }
+}
