d415d984cfb099e76957f94371bed64e1c186ea3
Alirio Rivera <ariveracu@unal.edu.co>
2026-01-19 21:06:38 -0500

    testB

---
+++ main/TestB.java
@@ -0,0 +1,20 @@
+package main;
+
+public class TestB {
+
+    public TestB(){
+
+    }
+
+    public int testeo(){
+        return 3;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo......");
+        TestB b = new TestB();
+        System.out.println(b.testeo());
+
+    }
+    
+}
